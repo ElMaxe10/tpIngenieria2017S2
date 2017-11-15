43f2f8b6fd5bd6cbe88ac205ac88583ab8ff3963
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2017-11-15 09:23:55 -0300

    corredores dibujados en el mapa

--- src/loader.js
+++ src/loader.js
@@ -23,96 +23,90 @@ function bootstrap() {
 
   //----------------------------------------------------------------------------
 
-    // Ubicación de la UNGS.
-    var ungsLocation = [-34.5221554, -58.7000067];
+  // Ubicación de la UNGS.
+  var ungsLocation = [-34.5221554, -58.7000067];
 
-    // Creación del componente mapa de Leaflet.
-    var map = L.map("mapid").setView(ungsLocation, 15);
+  // Creación del componente mapa de Leaflet.
+  var map = L.map("mapid").setView(ungsLocation, 15);
 
-    // Agregamos los Layers de OpenStreetMap.
-    var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+  // Agregamos los Layers de OpenStreetMap.
+  var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-    // Agregamos el control para seleccionar Layers al mapa
-    var layersControl = L.control.layers({
-        "Base": baseLayer
-    });
-    layersControl.addTo(map);
-    // hack:
-    map.layersControl = layersControl;
+  // Agregamos el control para seleccionar Layers al mapa
+  var layersControl = L.control.layers({
+      "Base": baseLayer
+  });
+  layersControl.addTo(map);
+  // hack:
+  map.layersControl = layersControl;
 
-    // Creamos un círculo con centro en la UNGS.
-    //var circle = L.circle(ungsLocation, {
-    //    color: '#0000AA',
-    //    fillColor: '#0000CC',
-    //    fillOpacity: 0.2,
-    //    radius: 300
-    //}).addTo(map);
+  // Creamos un circuito.
+//  L.polyline([
+//      L.latLng(-34.524309,-58.695315),
+//      L.latLng(-34.521865, -58.698213),
+//      L.latLng(-34.520437, -58.699889),
+//      L.latLng(-34.522388, -58.701957),
+//      L.latLng(-34.523579, -58.700350)
+//  ], {color: 'red'}).addTo(map);
 
-    // Creamos un polígono.
-    //L.polygon([
-    //    L.latLng(-34.515594, -58.705654),
-    //    L.latLng(-34.523503, -58.714062),
-    //    L.latLng(-34.519177, -58.719890),
-    //    L.latLng(-34.511089, -58.711374),
-    //    L.latLng(-34.514062, -58.707909),
-    //    L.latLng(-34.513824, -58.707584),
-    //]).addTo(map);
+  //----------------------------------------------------------------------------
 
-    // Creamos un circuito.
-    L.polyline([
-        L.latLng(-34.524309,-58.695315),
-        L.latLng(-34.521865, -58.698213),
-        L.latLng(-34.520437, -58.699889),
-        L.latLng(-34.522388, -58.701957),
-        L.latLng(-34.523579, -58.700350)
-    ], {color: 'red'}).addTo(map);
+  // Creamos un marker sobre la UNGS.
+//  console.log("creando Maker sobre UNGS");
+//  var ungsMarker = L.marker(ungsLocation);
+//  ungsMarker.addTo(map);
 
-    //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-    // Creamos un marker sobre la UNGS.
-    console.log("creando Maker sobre UNGS");
-    var ungsMarker = L.marker(ungsLocation);
-    ungsMarker.addTo(map);
+  var formularioInicial = document.formulario;
 
-    //----------------------------------------------------------------------------
-
-    console.log("creando incidentsLoader");
-    var incidentsLoader = new IncidentsLoader(incidentsSource);
-    console.log("loadIncidents");
-    incidentsLoader.loadIncidents(map); // llama a la funcion loadIncidents de incidentsSource.js
-
-    // Creamos un pedido de viaje
-    var travelreq = new TravelRequest("UNGS", map);
-    var car1 = new CarDriver("Pepe", [
-            {lon: -58.695290, lat: -34.524297},
-            {lon: -58.697030, lat: -34.522856},
-            {lon: -58.698210, lat: -34.521874}
-        ]);
-    travelreq.addCar(car1);
-    var car2 = new CarDriver("Bolt", [
-            {lon: -58.702329, lat: -34.522739},
-            {lon: -58.702572, lat: -34.522992},
-            {lon: -58.702801, lat: -34.523191},
-            {lon: -58.703056, lat: -34.523412},
-            {lon: -58.703299, lat: -34.523643}
-        ]);
-    travelreq.addCar(car2);
-
-    //----------------------------------------------------------------------------
-
-    var formularioInicial = document.formulario;
-
-    //agreagar texto
-    var div = document.getElementById("conductores");
-    div.textContent = "Conductores";
-
-    $("#myBtn").click(function(){
-      console.log("se hizo click en enviar solicitud");
-      travelreq.start();// START!
+  $("#myBtn").click(function(){
+    console.log("se hizo click en enviar solicitud");
+    if (cargaFinalizada()) {
       document.getElementById("formularioInicial").style.visibility='hidden';
       document.getElementById('conductores').style.visibility='visible';
-    });
+      travelreq.start();// START!
+      //$("#drivers").show(500); // esta linea no hacia falta porque esta en el driver.js
+    } else {
+        console.log("carga incompleta");
+        // un warning de bootstrap para indicar que la carga no ha finalizado
+        var alert = "<div class=\"alert alert-warning alert-dismissable\">" +
+            "\<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>" +
+            "<strong>Atención!</strong> No se han cargado todos los elementos en el mapa.</div>";
+        $("#aviso").append(alert).show(500); //y aca le decimos a jquery que cargue el cartel en el html
+    }
+  });
+
+  //determina si la carga de los datos finalizo o no.
+  function cargaFinalizada() {
+      return travelreq.finishedLoad && driversLoader.finishedLoad && positionsLoader.finishedLoad && incidentsLoader.finishedLoad;
+  }
+
+  //----------------------------------------------------------------------------
+
+  //creamos el viaje, los conductores, las posiciones y los dibujamos
+  console.log("creando Viaje");
+  var travelreq = new TravelRequest("Pedido a ungs", map);
+
+  //creamos los conductores y las posiciones
+  console.log("creando Driversloader");
+  var driversLoader = new DriversLoader(driversSource);
+  console.log("creando Positionsloader");
+  var positionsLoader = new PositionsLoader(positionsSource);
+
+  // cargamos los conductores con sus posiciones
+  console.log("loadDrivers");
+  driversLoader.loadDriversTo(travelreq, positionsLoader);
+
+  //creamos y cargamos las incidencias
+  console.log("creando incidentsLoader");
+  var incidentsLoader = new IncidentsLoader(incidentsSource);
+  console.log("loadIncidents");
+  incidentsLoader.loadIncidents(map); // llama a la funcion loadIncidents de incidentsSource.js
+
+  //----------------------------------------------------------------------------
+
 }
 $(bootstrap);
--- src/travelRequest.js
+++ src/travelRequest.js
@@ -1,44 +1,78 @@
 var TravelRequest = function(name, map) {
-    this.name = name;
-    this.map = map;
-    this.carsData = [];
+    this.name = name; // nombre de la carrera
+    this.map = map; // mapa de la carrera
+    this.driversData = []; //arreglo de conductores, aca se agregan instancias de driver.
+    this.finishedLoad; // en un boolean, como es null, te lo devuelve con false por defecto
 
-    this.addCar = function(carDriver) {
-        //Creamos el layer en el mapa para ese carDriver
-        var carLayer = L.layerGroup().addTo(this.map);
+    // dada una posición, busca el conductor al que le corresponde
+    // y la asocia al mismo.
+    this.bindDriverPosition = function(driverPosition) {
+        this.driversData.forEach(function(data) {
+            var driver = data.driver;
+            if (driver.id == driverPosition.id) {
+
+                driverPosition.positions.forEach(function(position) {
+                    driver.addPosition(position.lat, position.lon);
+                });
+
+                console.log("posiciones asociadas a " + driver.showDetails() + " posiciones:");
+                driver.historyPositions.forEach(function(position) {
+                    console.log(position);
+                });
+            }
+        });
+        this.finishedLoad = true;
+    }
+
+    this.addDriver = function(driver) {
+        // Creamos el layer en el mapa para ese conductor
+        var driverLayer = L.featureGroup().addTo(this.map); // es un featureGroup
+        // porque el mismo permite bindear comportamiento a todos los elementos del layer.
+        driverLayer.bindPopup(driver.showDetails()); // bindeo de un popup a todos los markers del grupo.
         // Agregamos el layer al control
-        this.map.layersControl.addOverlay(carLayer, carDriver.name);
-
+        this.map.layersControl.addOverlay(driverLayer, driver.name);
+        // funcion que dibuja al conductor en el mapa.
         var updater = function(newPosition) {
-            console.log("Updating view for car driver: " + carDriver.name + "!!");
-            console.log(newPosition);
+            if (typeof newPosition != "undefined") {
+                console.log("Updating view for driver: " + driver.name + "!!");
+                console.log(newPosition);
+                // borando los markers ya mostrados.
+                driverLayer.clearLayers();
 
-            carLayer.clearLayers();
-
-
-            // Opción 1.
-            //carLayer.addLayer(L.marker(newPosition));
-            // Opción 2.
-             carLayer.addLayer(L.circleMarker(newPosition, {
-                                     radius: 7,
-                                     fillColor: "#00AA00",
-                                     color: "black",
-                                     weight: 1,
-                                     opacity: 1,
-                                     fillOpacity: 0.3
-              }));
+                //actualizando la posiciones del conductor en el mapa.
+                driverLayer.addLayer(L.circleMarker(newPosition, {
+                    radius: 10,
+                    fillColor: "#00AA00",
+                    color: "black",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.5
+                }));
+            }
         }
 
-        this.carsData.push({
-            carDriver: carDriver,
+        // agregar a la carrera cada conductores, con el updater asociado
+        this.driversData.push({
+            driver: driver,
             updater: updater
         })
     }
 
+    // funcion que carga el listado de conductores en el documento html
+    this.loadListOfDrivers = function() {
+        console.log("cargando lista de conductores length: " + this.driversData.length);
+        this.driversData.forEach(function(data) {
+            var driver = data.driver;
+            var itemList = "<a class=\"list-group-item\"><h4 class=\"list-group-item-heading\">" + driver.name + " " + driver.surname + "</h4><p class=\"list-group-item-text\">" + driver.score + "</p></a>";
+            $("#Conductores").append(itemList);
+        });
+    }
+
+    // toma cada uno de esos conductores que estan en driversdata y lo dibuja en el mapa
     this.start = function() {
-        this.carsData.forEach(function(data) {
-            var carDriver = data.carDriver;
-            carDriver.move(data.updater);
+        this.driversData.forEach(function(data) {
+            var driver = data.driver;
+            driver.travel(data.updater);
         });
     }
 };
